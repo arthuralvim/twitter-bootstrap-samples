5695790bc4485afc5911346378b9f571deb1eae3
Arthur Alvim <arth.alvim@gmail.com>
2014-06-30 17:40:35 -0300

    Uploading new layouts for conference.

--- conference-examples/js/conference.js
+++ conference-examples/js/conference.js
@@ -12,6 +12,7 @@ var SITE = {
         this.go_to_top();
         this.menu_opacity();
         this.header_opacity();
+        this.timeline();
     },
 
     parallax: function(){
@@ -94,6 +95,23 @@ var SITE = {
             };
         });
 
+    },
+
+    timeline: function(){
+        var my_posts = $("[rel=tooltip]");
+
+        var size = $(window).width();
+        for(i=0;i<my_posts.length;i++){
+            the_post = $(my_posts[i]);
+
+            if(the_post.hasClass('invert') && size >=767 ){
+                the_post.tooltip({ placement: 'left'});
+                the_post.css("cursor","pointer");
+            }else{
+                the_post.tooltip({ placement: 'rigth'});
+                the_post.css("cursor","pointer");
+            }
+        }
     },
 
 };
